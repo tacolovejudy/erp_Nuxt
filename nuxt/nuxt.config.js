a4f133ad8c01d68923d4d49c1660816b05d41084
tacolovejudy <tacolovejudy@gmail.com>
2020-06-09 16:59:27 +0800

    init

---
+++ nuxt/nuxt.config.js
@@ -0,0 +1,67 @@
+
+export default {
+
+  mode: 'universal',
+  server: {
+    port: 3007, // default: 3000
+    host: '127.0.0.1' // default: localhost,
+  },
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: process.env.npm_package_name || '',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+  /*
+  ** Customize the progress-bar color
+  */
+  loading: { color: '#fff' },
+  /* 
+  ** Global CSS
+  */
+  css: [
+    '~/assets/css/transition.css',
+  ],
+  /*
+  ** Plugins to load before mounting the App
+  */
+  plugins: [
+  ],
+  /*
+  ** Nuxt.js dev-modules
+  */
+  buildModules: [
+  ],
+  /*
+  ** Nuxt.js modules
+  */
+  modules: [
+    // Doc: https://bootstrap-vue.js.org
+    'bootstrap-vue/nuxt',
+    '@nuxtjs/axios',
+
+  ],
+  axios: {
+    baseURL: 'http://127.0.0.1:8001/api',
+    // browserBaseURL: "http://localhost",
+    //   // headers: { 'Content-Type': 'application/json' },
+  },
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** You can extend webpack config here
+    */
+    extend(config, ctx) {
+    }
+  }
+}
